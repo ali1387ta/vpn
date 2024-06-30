2d4ad58191954d64dfbe32793248e4ff58c2217d
ali1387ta <minecraftali1387t@gmail.com>
2024-06-30 14:16:41 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const childProcess = require("child_process");
+const http = require("http")
 
+http.createServer((req,res)=>{
+    res.write("alive")
+    res.end()
+}).listen(3000)
+setInterval(()=>{
+    fetch("https://vpn-bkyy.onrender.com")
+},10000)
 
 const res = childProcess.execSync("sudo wget -qO- https://raw.githubusercontent.com/Jigsaw-Code/outline-server/master/src/server_manager/install_scripts/install_server.sh | bash").toString()
 console.log(res)
