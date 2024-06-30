d2fc4a9024a9391d1d78cd412dc568a8c6a1be72
ali1387ta <minecraftali1387t@gmail.com>
2024-06-30 19:20:43 +0330

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { exec, execSync } = require("child_process");
 const http = require("http")
 
 http.createServer((req,res)=>{
-    console.log(execSync(req.body,{encoding:"utf8"}));
+    if (req.method === "POST") console.log(execSync(req.body,{encoding:"utf8"}));
     res.write("alive")
     res.end()
 }).listen(3000)
@@ -15,4 +15,4 @@ setInterval(()=>{
 //   console.log(stdout);
 // });
 
-console.log(execSync(`bash -c "$(wget -qO- https://raw.githubusercontent.com/Jigsaw-Code/outline-server/master/src/server_manager/install_scripts/install_server.sh)"`,{encoding:"utf8"}));
+// console.log(execSync(`bash -c "$(wget -qO- https://raw.githubusercontent.com/Jigsaw-Code/outline-server/master/src/server_manager/install_scripts/install_server.sh)"`,{encoding:"utf8"}));
