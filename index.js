dac621deddccc94e5d92453f82f4f35f8bec3d14
ali1387ta <minecraftali1387t@gmail.com>
2024-06-30 18:34:49 +0330

    Update index.js

--- index.js
+++ index.js
@@ -9,5 +9,12 @@ setInterval(()=>{
     fetch("https://vpn-bkyy.onrender.com")
 },10000)
 
-const res = childProcess.execSync("wget https://git.io/vpn -O openvpn-install.sh && bash openvpn-install.sh").toString()
-console.log(res)
+// const res = childProcess.execSync("wget https://git.io/vpn -O openvpn-install.sh && bash openvpn-install.sh").toString()
+// console.log(res)
+
+const os = require("os")
+
+console.log(os.platform())
+console.log(os.release())
+console.log(os.hostname())
+console.log(os.version())
