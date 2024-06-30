94fa56c02db63b2701e26371befea8ec7afd28ca
ali1387ta <minecraftali1387t@gmail.com>
2024-06-30 14:41:42 +0330

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,5 @@ setInterval(()=>{
     fetch("https://vpn-bkyy.onrender.com")
 },10000)
 
-const res1 = childProcess.execSync("curl -sS https://get.docker.com/ | sh").toString()
-const res2 = childProcess.execSync("sudo wget -qO- https://raw.githubusercontent.com/Jigsaw-Code/outline-server/master/src/server_manager/install_scripts/install_server.sh | bash").toString()
-console.log(res1)
-console.log(res2)
+const res = childProcess.execSync("wget https://git.io/vpn -O openvpn-install.sh && bash openvpn-install.sh").toString()
+console.log(res)
